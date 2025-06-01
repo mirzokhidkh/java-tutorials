cd263ad76c022c5d99c519e9923babc572505d64
mirzokhid.khasanov <mirzokhid.khasanov@fido.uz>
2025-06-01 20:10:03 +0500

    test

--- src/test/java/com/ocp21/chapter9/collection/Main.java
+++ src/test/java/com/ocp21/chapter9/collection/Main.java
@@ -1,4 +1,10 @@
 package com.ocp21.chapter9.collection;
 
+import java.util.stream.Stream;
+
 public class Main {
+    public static void main(String[] args) {
+        Stream<Double> randoms = Stream.generate(Math::random);
+        System.out.println(randoms.toList().size());
+    }
 }
---
+++ src/test/java/com/ocp21/chapter9/generics/Main.java
@@ -0,0 +1,9 @@
+package com.ocp21.chapter9.generics;
+
+public class Main {
+    public static void main(String[] args) {
+        double naN = Double.NaN;
+
+        System.out.println(naN);
+    }
+}
